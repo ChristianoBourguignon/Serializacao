5021d6f58cd571b330632f90e814147bf203dace
ChristianoBourguignon <christianombl@gmail.com>
2025-03-27 09:25:17 -0300

    Upload project

---
+++ src/Pessoa.java
@@ -0,0 +1,34 @@
+public class Pessoa {
+    public String nome;
+    private int idade;
+    private String telefone;
+    public Pessoa(String nome, int idade, String telefone){
+        this.nome = nome;
+        this.idade = idade;
+        this.telefone = telefone;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
